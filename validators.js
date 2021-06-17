7192b9021c4f2c79848b0a72c6858eac10b0d9d7
Diego Kleinman <diegokleinman1@gmail.com>
2021-06-17 10:49:48 -0300

    validator structure

--- validators.js
+++ validators.js
@@ -9,31 +9,11 @@ module.exports = {
     storageCommandValidator: (data) => {
         //Check for command, key, flags, exptime, bytes , optional: noreply
         if (data.length === 5 || data.length === 6) {
-            let key = data[1]
-            let flags = data[2]
-            let exptime = data[3]
-            let bytes = data[4]
-
-            //check max length of key
-            if (key.length > 250) {
-                return false
-            }
-            //Check flags 16 bits unsigned integer
-            else if ((flags % flags) !== 0 && parseInt(flags) !== 0 || parseInt(flags) > 65535) {
-                return false
-            }
-            //Check bytes integer // bytes less than max storage possible (1MB -- 1048576 bytes) // bytes not negative
-            else if ((bytes % bytes) !== 0 && parseInt(bytes) !== 0 || parseInt(bytes) > 1048576 || parseInt(bytes) < 0) {
-                return false
-            }
-            // Check exptime integer
-            else if ((exptime % exptime) !== 0 && parseInt(exptime) !== 0) {
-                return false
-            }
-            //If all checks are OK
-            else {
-                return true
-            }
+            const key = data[1]
+            const flags = data[2]
+            const exptime = data[3]
+            const bytes = data[4]
+            return validateParameters(key, flags, bytes, exptime)
         }
         return false
     },
@@ -45,38 +25,50 @@ module.exports = {
     casCommandValidator: (data) => {
         //Check for command, key, flags, exptime, bytes , cas , optional: noreply
         if (data.length > 5 && data.length <= 7) {
-            let key = data[1]
-            let flags = data[2]
-            let exptime = data[3]
-            let bytes = data[4]
-            let casValue = data[5]
+            const key = data[1]
+            const flags = data[2]
+            const exptime = data[3]
+            const bytes = data[4]
+            const cas = data[5]
 
-            //check max length of key
-            if (key.length > 250) {
-                return false
-            }
-            //Check flags 16 bits unsigned integer
-            else if ((flags % flags) !== 0 && parseInt(flags) !== 0 || parseInt(flags) > 65535) {
-                return false
-            }
-            //Check bytes integer // bytes less than max storage possible (1MB -- 1048576 bytes) // bytes not negative
-            else if ((bytes % bytes) !== 0 && parseInt(bytes) !== 0 || parseInt(bytes) > 1048576 || parseInt(bytes) < 0) {
-                return false
-            }
-            // Check exptime integer
-            else if ((exptime % exptime) !== 0 && parseInt(exptime) !== 0) {
-                return false
-            }
             // Check cas integer
-            else if ((casValue % casValue) !== 0 && parseInt(casValue) !== 0) {
+            if ((cas % cas) !== 0 && parseInt(cas) !== 0) {
                 return false
             }
-            //If all checks are OK
-            else {
-                return true
-            }
+            return validateParameters(key, flags, bytes, exptime)
         }
         return false
     }
 
+}
+
+/**
+ * This function validates parameters in a command input
+ * @param {string} key Key parameter
+ * @param {string} flags Flags parameter
+ * @param {string} bytes Bytes parameter
+ * @param {string} exptime Exptime parameter
+ * @returns true if parameters are valid, false otherwise
+ */
+const validateParameters = (key, flags, bytes, exptime) => {
+    //check max length of key
+    if (key.length > 250) {
+        return false
+    }
+    //Check flags 16 bits unsigned integer
+    else if ((flags % flags) !== 0 && parseInt(flags) !== 0 || parseInt(flags) > 65535) {
+        return false
+    }
+    //Check bytes integer // bytes less than max storage possible (1MB -- 1048576 bytes) // bytes not negative
+    else if ((bytes % bytes) !== 0 && parseInt(bytes) !== 0 || parseInt(bytes) > 1048576 || parseInt(bytes) < 0) {
+        return false
+    }
+    // Check exptime integer
+    else if ((exptime % exptime) !== 0 && parseInt(exptime) !== 0) {
+        return false
+    }
+    //If all checks are OK
+    else {
+        return true
+    }
 }
